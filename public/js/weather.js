b39551931f7e2500060a9d1a472aef3067b46c77
Bassem Shaker <bassemreda@gmail.com>
2013-04-11 17:00:50 +0100

    different app structure

--- public/js/weather.js
+++ public/js/weather.js
@@ -14,7 +14,7 @@
           $('body').addClass('install').html('<div id="install"><div id="homescreen"><span></span><h2 id="add">Add to your <strong>Home Screen</strong></h2></div></div>');
         }
       } else {
-        $('body').addClass('weather').html('<h3>Checking the weather...</h3><div class="loading" />');
+        $('body').addClass('weather').html('<div id="weathereye"><div id="frame"><div id="layer"><div class="slide"><h3>Checking the weather...</h3><div class="loading" /></div></div></div></div>');
         this.forcastURL = "http://weathereye.co/forcast/";
         this.yahooAppId = "pmQ_VnzV34FddFT6do_XVxcjzkrjmeKzNpJjLP1MqfPSEN6yCN0vunwBt8QbZYWEc65EPzD6o8VVmDYXTQZbPY0DkXSGUO4-";
         this.yahooURL = "http://where.yahooapis.com/v1/places.q('[place')?appid=[appid]";
